57597fc56f95dd9f6b4c84ba31f1a5bc6fa9e085
philculver <pculver@nc2media.com>
2019-04-15 19:58:35 -0500

    Add Number List detail fragment

---
+++ app/src/main/java/dev/codephoenix/abcapp/ui/main/numbers/NumberPageFragment.kt
@@ -0,0 +1,23 @@
+package dev.codephoenix.abcapp.ui.main.numbers
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import dev.codephoenix.abcapp.R
+
+class NumberPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_number_page, container, false)
+    }
+
+
+}
